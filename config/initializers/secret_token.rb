7b3ffd88bdf3f9042bbf797f7b63c67739844960
elliottm <m_elliott@live.com>
2013-11-18 14:24:05 +0000

    first rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BloggingApp::Application.config.secret_key_base = '17d316a7f8747d48209d9b19440dbb54f63ae5955962c51d763729e1fca2d778e7faaa11ba017708323c083922e95677c1830c6c531c7ceb0470ed105c8c6da9'
---
+++ features/welcome_page.feature
@@ -0,0 +1,11 @@
+Feature:
+
+In order to start using the website
+As a user
+I want to be on the homepage
+
+Scenario:
+
+Given I am on the homepage
+Then I should see "Welcome"
+
